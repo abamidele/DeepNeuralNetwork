d6588b4e5e5e0f3a3cdf29c02fb69ef644a506e6
abamidele <ajayi.bamidele1231@gmail.com>
2024-09-28 10:58:57 -0400

    Update PythonCodeForSingleNeuron.py

--- PythonCodeForSingleNeuron.py
+++ PythonCodeForSingleNeuron.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 import numpy as np
 
@@ -63,7 +61,7 @@ if __name__ == "__main__":
         print(f"Input: {X[i]}, Predicted Output: {neuron.predict(X[i])}")
 
 
-# In[ ]:
+
 
 
 
